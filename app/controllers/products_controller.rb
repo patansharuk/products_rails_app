64a3734a8f4a5dc8841081f231c1c4b01a49b856
sharukhan <sharukhan@webkorps.com>
2024-05-14 21:43:45 +0530

    crud operations in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,50 @@
 class ProductsController < ApplicationController
+    protect_from_forgery with: :null_session
+
     def index
         @products = Product.all
-        respond_to do |format|
-            # format.html
-            # format.xml { render xml: @products }
-            format.json { render json: @products }
+        render json: @products
+    end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            render json: @product
+        else
+            render json: {message: 'Failed creating product!'}
         end
+    end
+
+    def update
+        @product = Product.find_by(id: params[:id])
+        if @product
+            if @product.update(product_params)
+                render json: @product
+            else
+                render json: {message: @product.errors.full_messages}
+            end
+        else
+            render json: {message: 'product with id not found'}
+        end
+        
+    end
+
+    def destroy
+        @product = Product.find_by(id: params[:id])
+        if @product
+            if @product.destroy
+                render json: @product
+            else
+                render json: {message: @product.errors.full_messages}
+            end
+        else
+            render json: { message: 'Product with id not found' }
+        end
+    end
+
+    private
+
+    def product_params
+        params[:product].permit(:title, :description)
     end
 end
