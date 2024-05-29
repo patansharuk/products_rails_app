3c6c3c2c8ed240490b999187903216eca94398cf
sharukhan <sharukhan@webkorps.com>
2024-05-29 20:57:08 +0530

    fixed decode for invalid token

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -36,18 +36,22 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_to_on_destroy
     if request.headers['Authorization'].present?
-      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
-      current_user = User.find(jwt_payload['sub'])
-    end
-    
-    if current_user
-      render json: {
-        message: 'Logged out successfully.'
-      }, status: :no_content
+      begin
+        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+        current_user = User.find(jwt_payload['sub'])
+      rescue
+        render json: {message: 'Invalid or Expired token!'}, status: :no_content
+      end
     else
-      render json: {
-        message: "Couldn't find an active session."
-      }, status: :unauthorized
+      if current_user
+        render json: {
+          message: 'Logged out successfully.'
+        }, status: :no_content
+      else
+        render json: {
+          message: "Couldn't find an active session."
+        }, status: :unauthorized
+      end
     end
   end
 end
